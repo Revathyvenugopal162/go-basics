131194c2aa0f33d90e87457adc67450e98223c37
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-10-12 11:20:45 +0200

    Add branching initial

--- main.go
+++ main.go
@@ -1,9 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
-	for i := 1; i < 5; i++ {
-		fmt.Println(i)
+	type mystr struct {
+		name  string
+		price map[string]float64
+	}
+	menu := []mystr{
+		{name: "coffee", price: map[string]float64{"small": 10, "medium": 20, "large": 30}},
+		{name: "tea", price: map[string]float64{"small": 10, "medium": 20, "large": 30}},
+	}
+loop:
+	for {
+		in := bufio.NewReader(os.Stdin)
+		fmt.Println(("pelsea select an item in the menu"))
+		fmt.Println("1) print menu")
+		fmt.Println("2) add item")
+		fmt.Println("3) q")
+		choice, _ := in.ReadString(('\n'))
+
+		switch strings.TrimSpace(choice) {
+		case "1":
+			for _, item := range menu {
+				fmt.Println(item.name)
+				fmt.Println(strings.Repeat("-", 10))
+				for size, cost := range item.price {
+					fmt.Printf("\t%10s%10.2f\n", size, cost)
+				}
+			}
+		case "2":
+			{
+				fmt.Println("enter the item")
+				name, _ := in.ReadString('\n')
+				menu = append(menu, mystr{name: name, price: make(map[string]float64)})
+				fmt.Println()
+			}
+		case "3":
+			{
+				break loop
+			}
+
+		default:
+			fmt.Println(("invalid"))
+
+		}
 	}
 }
